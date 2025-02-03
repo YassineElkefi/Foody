89ce1d1a8b62ca03fa1ac6fb6c3a784dc3388bc7
YassineElkefi <yassine.elkefi6@gmail.com>
2025-02-03 12:17:29 +0100

    Recipe GraphQL initial APIs

--- graphql_backend/src/graphql/resolvers.js
+++ graphql_backend/src/graphql/resolvers.js
@@ -1,8 +1,13 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const Recipe = require('../models/Recipe');
 const { generateToken } = require('../utils/auth');
+const { Query, get } = require('mongoose');
+const { GraphQLError } = require('graphql');
 module.exports = {
     Query: {
+
+        //Users Queries
         me: async (_, __, { user }) => {
             if(!user) throw new Error('You are not authenticated');
             return await User.findById(user.id);
@@ -10,10 +15,15 @@ module.exports = {
         users: async (_, __ , { user }) =>{
             if (!user || user.role !== "ROLE_ADMIN" ) throw new Error('Aceess Denied');
             return await User.find();
-        }
+        },
+
+        //Recipes Queries
+        getRecipes: async () => await Recipe.find().populate('createdBy'),
+        getRecipe: async (_, { id }) => await Recipe.findById(id).populate('createdBy'),
     },
 
     Mutation: {
+        //User Mutations
         register: async (_, { username, email, password}) =>{
             const existingUser = await User.findOne( { email });
             if(existingUser) throw new Error('User already exists');
@@ -40,5 +50,22 @@ module.exports = {
             const token = generateToken(user);
             return { ...user._doc, id: user._id, token};
         },
+
+        //Recipe Mutations
+
+        addRecipe: async (_, { title, description, ingredients, steps}, {user}) =>{
+            if (!user || user.role !== "ROLE_USER") throw new GraphQLError("Only users can add recipes", { extensions: { code: "FORBIDDEN"}});
+
+            const recipe = new Recipe({
+                title,
+                description,
+                ingredients,
+                steps,
+                createdBy: user.id,
+                createdAt: new Date().toISOString(),
+            });
+            await recipe.save();
+            return recipe.populate('createdBy');
+        }
     },
 };
--- graphql_backend/src/graphql/typeDefs.js
+++ graphql_backend/src/graphql/typeDefs.js
@@ -10,13 +10,30 @@ module.exports = gql`
         token: String
     }
 
+    type Recipe{
+        id: ID!
+        title : String!
+        description: String!
+        ingredients: [String]!
+        steps: [String]!
+        createdBy: User!
+        createdAt: String!
+    }  
+    
     type Query{
         me: User
         users: [User] #Admin Only 
+        getRecipes: [Recipe]!
+        getRecipe(id: ID!): Recipe
     }
 
     type Mutation{
         register(username: String!, email: String!, password: String!): User
         login(email: String!, password: String!): User
+
+        addRecipe(title: String!, description: String!, ingredients: [String]!, steps: [String]!): Recipe
+        deleteRecipe(id: ID!): Recipe
     }
+
+    
 `;
